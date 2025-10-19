b1269f170bda5346ea0b7d0a38241e85a43f19d5
Priyanshu Soni <priyanshu171203@gmail.com>
2025-10-19 19:06:20 +0530

    completed data parsing implementation and testing,gotta test it out on a larger data

--- wiki-utils/src/main/java/com/pri1712/searchengine/wikiutils/WikiDocument.java
+++ wiki-utils/src/main/java/com/pri1712/searchengine/wikiutils/WikiDocument.java
@@ -1,16 +1,22 @@
 package com.pri1712.searchengine.wikiutils;
 
+import java.util.logging.*;
+
 public class WikiDocument {
     private String id;
     private String title;
-    private StringBuilder text;
+    private String text;
     private String timestamp;
-
-    public WikiDocument(String id, String title, StringBuilder text, String timestamp) {
+    private static final Logger LOGGER = Logger.getLogger(WikiDocument.class.getName());
+    public WikiDocument(String id, String title, String text, String timestamp) {
         this.id = id;
         this.title = title;
         this.text = text;
         this.timestamp = timestamp;
+        LOGGER.log(Level.INFO, "WikiDocument created with title: {0}", title);
+        LOGGER.log(Level.INFO, "WikiDocument created with text: {0}", text);
+        LOGGER.log(Level.INFO, "WikiDocument created with timestamp: {0}", timestamp);
+        LOGGER.log(Level.INFO, "WikiDocument created with id: {0}", id);
     }
 
     public String getTimestamp() {
@@ -21,11 +27,11 @@ public class WikiDocument {
         this.timestamp = timestamp;
     }
 
-    public StringBuilder getText() {
+    public String getText() {
         return text;
     }
 
-    public void setText(StringBuilder text) {
+    public void setText(String text) {
         this.text = text;
     }
 
