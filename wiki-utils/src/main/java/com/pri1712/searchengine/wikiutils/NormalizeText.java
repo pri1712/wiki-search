02dd157f0f322b3479c78d5319c029b1a07e4322
Priyanshu Soni <priyanshu171203@gmail.com>
2025-10-20 08:01:57 +0530

    validating parsing correctness

--- wiki-utils/src/main/java/com/pri1712/searchengine/wikiutils/NormalizeText.java
+++ wiki-utils/src/main/java/com/pri1712/searchengine/wikiutils/NormalizeText.java
@@ -1,9 +1,17 @@
 package com.pri1712.searchengine.wikiutils;
 
+import java.util.logging.*;
 public class NormalizeText {
+
+    private static Logger LOGGER = Logger.getLogger(NormalizeText.class.getName());
 
     public StringBuilder cleanText(StringBuilder text) {
         String lowercaseText = text.toString().toLowerCase();
+//        LOGGER.log(Level.INFO,"Clean text: {0}", lowercaseText);
+        if (lowercaseText.matches("(?i)^#redirect\\s*\\[\\[.*\\]\\]")) {
+            LOGGER.log(Level.INFO,"has a redirect text");
+            return new StringBuilder();
+        }
         String cleanText = lowercaseText.replaceAll("[^a-z0-9\\s]"," ");
         cleanText = cleanText.replaceAll("\\s+", " ").trim();
         StringBuilder cleanSb =new StringBuilder(cleanText);
