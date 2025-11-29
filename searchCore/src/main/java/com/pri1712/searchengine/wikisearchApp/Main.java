4e7166f29aca8571bfb1dfa4fa66ca57a2dc9749
Priyanshu Soni <priyanshu171203@gmail.com>
2025-11-29 18:18:19 +0530

    working on query engine

--- searchCore/src/main/java/com/pri1712/searchengine/wikisearchApp/Main.java
+++ searchCore/src/main/java/com/pri1712/searchengine/wikisearchApp/Main.java
@@ -22,7 +22,7 @@ public class Main {
     static String tokenizedFilePath = TOKENIZED_FILE_PATH;
     static String indexedFilePath = INDEXED_FILE_PATH;
     static String tokenIndexOffsetPath = TOKEN_INDEX_OFFSET_PATH;
-
+    private static String READ_MODE = System.getenv("READ_MODE");
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter path to Wikipedia XML dump file: ");
@@ -30,43 +30,53 @@ public class Main {
         scanner.close();
         long startTime = System.nanoTime();
         //parser
-        try {
-            Parser parser = new Parser(dataFilePath);
-            parser.parseData();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        //tokenizer
-        try {
-            Tokenizer tokenizer = new Tokenizer();
-            LOGGER.info("Tokenizing Wikipedia XML dump file: " + parsedFilePath);
-            tokenizer.tokenizeData(parsedFilePath);
+        if (READ_MODE.equals("false")) {
+            try {
+                Parser parser = new Parser(dataFilePath);
+                parser.parseData();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            //tokenizer
+            try {
+                Tokenizer tokenizer = new Tokenizer();
+                LOGGER.info("Tokenizing Wikipedia XML dump file: " + parsedFilePath);
+                tokenizer.tokenizeData(parsedFilePath);
 
-        } catch (RuntimeException e) {
-            LOGGER.log(Level.WARNING, e.getMessage());
-            throw new RuntimeException(e);
-        }
-        //indexer
-        try {
-            IndexWriter indexWriter = new IndexWriter(indexedFilePath);
-            LOGGER.info("Indexing Wikipedia XML dump file: " + tokenizedFilePath);
-            indexWriter.indexData(tokenizedFilePath);
-            indexWriter.mergeAllIndexes(indexedFilePath);
-        } catch (RuntimeException | IOException e) {
-            throw new RuntimeException(e);
+            } catch (RuntimeException e) {
+                LOGGER.log(Level.WARNING, e.getMessage());
+                throw new RuntimeException(e);
+            }
+            //indexer
+            try {
+                IndexWriter indexWriter = new IndexWriter(indexedFilePath);
+                LOGGER.info("Indexing Wikipedia XML dump file: " + tokenizedFilePath);
+                indexWriter.indexData(tokenizedFilePath);
+                indexWriter.mergeAllIndexes(indexedFilePath);
+            } catch (RuntimeException | IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            //load indexes for querying
+            try {
+                IndexReader indexReader = new IndexReader(indexedFilePath,tokenIndexOffsetPath);
+                IndexData indexData = indexReader.readTokenIndex(TEST_TOKEN);
+                LOGGER.fine("Read data from inverted index for token " + TEST_TOKEN);
+                LOGGER.fine("DocIds " + indexData.getDocIds());
+                LOGGER.fine("frequencies " + indexData.getFreqs());
+            } catch (RuntimeException | IOException e) {
+                LOGGER.log(Level.WARNING, e.getMessage());
+                throw new RuntimeException(e);
+            }
+
+            try {
+
+            } catch (RuntimeException e) {
+                throw new RuntimeException(e);
+            }
         }
 
-        //querying is the next step.
-        try {
-            IndexReader indexReader = new IndexReader(indexedFilePath,tokenIndexOffsetPath);
-            IndexData indexData = indexReader.readTokenIndex(TEST_TOKEN);
-            LOGGER.fine("Read data from inverted index for token " + TEST_TOKEN);
-            LOGGER.fine("DocIds " + indexData.getDocIds());
-            LOGGER.fine("frequencies " + indexData.getFreqs());
-        } catch (RuntimeException | IOException e) {
-            LOGGER.log(Level.WARNING, e.getMessage());
-            throw new RuntimeException(e);
-        }
+        //loading the indexes for querying.
         long endTime = System.nanoTime();
         long elapsedTime = endTime - startTime;
         LOGGER.log(Level.INFO,"Time taken to parse the data : {0} ms",elapsedTime/100000);
